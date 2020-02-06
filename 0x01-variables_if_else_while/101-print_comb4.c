072b8e6ab7fcd0436344767cf5a632e69d9d8451
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-06 22:35:49 +0000

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 48;
+	j = 49;
+	k = 50;
+	while  ((i < 56) && (j < 57) && (k < 58))
+	{
+		putchar(i);
+		putchar(j);
+		putchar(k);
+		if ((i == 55) && (j == 56) && (k == 57))
+		{
+			putchar('\n');
+			i++;
+			j++;
+			k++;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			if (k < 57)
+			{
+				k++;
+			}
+			else
+			{
+				if (j < 56)
+				{
+					j++;
+					k = j + 1;
+				}
+				else
+				{
+					i++;
+					j = i + 1;
+				}
+			}
+		}
+
+	}
+
+	return (0);
+}
