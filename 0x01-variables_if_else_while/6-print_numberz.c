989516da6d589eaf5002a5ec1411305fb6df9f5c
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-06 16:30:00 +0000

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
