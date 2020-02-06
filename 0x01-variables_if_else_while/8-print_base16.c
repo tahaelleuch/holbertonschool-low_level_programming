d0a475ddc0bf4685136ceb6d09e21f8b0887525b
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-06 16:41:55 +0000

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+	a = 97;
+	while (a < 103)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
