b3ef871f69b8340605e4a1396e9c1ccf0447af0e
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-06 22:16:24 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 48;
+	j = 49;
+	while  ((i < 57) && (j < 58))
+	{
+		putchar(i);
+		putchar(j);
+		if ((i == 56) && (j == 57))
+		{
+			putchar('\n');
+			i++;
+			j++;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			if (j < 57)
+			{
+				j++;
+			}
+			else
+			{
+				i++;
+				j = i + 1;
+			}
+		}
+
+	}
+
+	return (0);
+}
