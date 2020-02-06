f334542188b2edc9950a62e196e7705696f2a18e
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-06 12:25:29 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description : using the function "putchar" to print all the aplhbaets
+ * Return: 0
+ */
+int main (void)
+{
+	char ch = 'a';
+
+	while (c <= 'z')
+	{
+		putchar (ch);
+		ch = ch + 1;
+	}
+	putchar ("\n");
+
+	return (0);
+}
