14f59940ee779cfe3bf3a7dd574ba097da73fd31
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-11 18:11:15 +0000

    102-fibonacci.c

---
+++ 0x03-debugging/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - block
+ * Description: print the 50 Fibonacci numbers, starting with 1 and 2
+ * Return: 0 always
+*/
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int c;
+	int i;
+	printf("%d",a);
+	printf("%d",b);
+	for (i = 0; i < 50; i++)
+	{
+		c = a + b;
+		printf("%d",c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return(0);
+}
