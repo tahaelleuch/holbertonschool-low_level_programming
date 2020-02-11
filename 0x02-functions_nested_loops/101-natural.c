52c6d9a2a3516c2742818532b114180314329068
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-11 13:29:35 +0000

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - main block
- * Description: Print the sum of all multiples of 3 or 5 below 1024.
- * Return: 0
-*/
+ * main - natural numbers multiples of 3 or 5 to 1024
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	int sum = 0;
 	int i;
 
-	for (i = 0; i <= 1024; i++)
+	for (i = 1; i <= 1024; i++)
 	{
 		if ((i % 3) == 0)
 			sum = sum + i;
