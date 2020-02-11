4e2aee9f1fbb668fc6e5d3eb1443cc2b38ea0c44
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-11 13:53:57 +0000

    first

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *  main - tells you if random number is negative or positive
+ *
+ *  Description: Uses if else to print if # if neg/pos/zero
+ *  Return: returns zero
+*/
+int positive_or_negative(int n)
+{
+
+/*	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+*/
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+
+	return (0);
+}
