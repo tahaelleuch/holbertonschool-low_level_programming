7bb1bdfcfbdfed78f38dc2327ed7d3c27892a519
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-11 08:15:45 +0000

    last one

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
+
 /**
- * print_alphabet_x10 - prints the alphabet, ten times
- * @c integer
- * Return: Always 0.
+ * _islower - returns 1 if is a lowercase letter
+ *
+ * @c: character to check for lowercase
+ * Return: Returns 1 if a lowercase number, returns 0 if not
  */
 int _islower(int c)
 {
