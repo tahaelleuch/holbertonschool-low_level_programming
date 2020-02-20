926173d7f2396ad2a5eec88db8ec44493dd65417
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-20 13:03:32 +0000

    6-cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @c: char to capitalize
+ * Return: char @c capitalized
+*/
+char *cap_string(char *c)
+{
+	int i = 0;
+	int j;
+
+	while (c[i] != '\0')
+		i++;
+	for (j = 0; j < i; j++)
+	{
+		if ((c[j] == '\t' || c[j] == '\n' || c[j] == ',' || c[j] == ';' || c[j] == '.' || c[j] == '!' || c[j] == '?' || c[j] == '"' || c[j] == '(' || c[j] == ')' || c[j] == '{' || c[j] == '}' || c[j] == ' ') && (c[j + 1] >= 'a' && c[j + 1] <= 'z'))
+		{
+			c[j + 1] = c[j + 1] - 32;
+		}
+	}
+	return (c);
+}
