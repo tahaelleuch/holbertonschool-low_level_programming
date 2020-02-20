734aaa300101c04552f531d74b5cef483d19b80c
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-20 11:47:09 +0000

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strncpy - function that copies a string
+ * @dest: char dest
+ * @src: char source
+ * @n: int number of char to copy
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+		{
+		dest[i] = '\0';
+		i++;
+		}
+	return (dest);
+}
