63d05981b74694fe1206a9b884b250b49226d696
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-21 22:29:54 +0000

    102-infinite_add

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -5,39 +5,53 @@
  * @n2: char two
  * @r: char result
  * @size_r: int buffer size
+ * Return: char @r
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-        int i = 0, j = 0, k, digit, rest = 0, l;
+	int i = 0, j = 0, k, digit, rest = 0;
 
-        while (n1[i] != '\0')
-                i++;
-        while (n1[j] != '\0')
-                j++;
-        if (i >= size_r || j >= size_r)
-                return (0);
-        for (k = 0; k < size_r - 1; i--, j--, k++)
-        {
-                digit = rest;
-                if (i > 0)
-                        digit = digit + (n1[i] - '0');
-                if (j > 0)
-                        digit = digit + (n2[j] - '0');
-                if (i < 0 && j < 0 && digit == 0)
-                {
-                        break;
-                }
-                rest = digit / 10;
-                r[k] = digit % 10 + '0';
-        }
-        r[k] = '\0';
-        if (i >= 0 || j >= 0 || rest)
-                return (0);
-        for (k -= 1, l = 0; l < k; k--, l++)
-        {
-                rest = r[k];
-                r[k] = r[l];
-                r[l] = rest;
-        }
-        return (r);
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i >= j)
+		k = i;
+	else
+		k = j;
+	if (k + 1 >= size_r)
+		return (0);
+	i--, j--;
+	r[k + 1]= '\0';
+	k = k - 1;
+	while (k > 0)
+	{
+		digit = (n1[i] - '0') + (n2[j] - '0') + rest;
+		rest = digit / 10;
+		digit = digit % 10;
+		r[k] = (digit + '0');
+		i--, j--, k--;
+		if (i < 0)
+		{
+			i = 0;
+			n1[0] = '0';
+		}
+		if (j < 0)
+		{
+			j = 0;
+			n1[0] = '0';
+		}
+	}
+/**	while (k >= 0)
+	{
+		if (i >= 0)
+			r[k] = n1[i] + 1;
+		else
+			r[k] = n2[j];
+		i--, j--, k--;
+	}
+*/	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
