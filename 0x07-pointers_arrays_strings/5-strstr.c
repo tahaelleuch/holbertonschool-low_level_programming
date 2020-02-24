50437b19f1ce05f0e720ed7b51a9dafcbe95f871
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-24 13:22:24 +0000

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strstr - function that locates a substring
+ * @haystack: char to look in
+ * @needle: char to look for
+ * Return: char
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (k = 0; needle[k] != '\0'; k++)
+			{
+				if (haystack[i + k] == needle[k])
+					return (haystack + i);
+			}
+		}
+	}
+	return (0);
+}
