16c5c3337a368c48705a48a68874d2dc626a4cad
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-25 10:45:09 +0000

    9-

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -3,9 +3,9 @@
  * set_string - sets the value of a pointer to a char
  * @s: char from
  * @to: char to
- * Return: void
+ * Return: 0 Always
 */
 void set_string(char **s, char *to)
 {
-	*s = to;
+        *s = to;
 }
