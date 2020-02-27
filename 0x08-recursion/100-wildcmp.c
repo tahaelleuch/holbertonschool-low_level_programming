2adb5f114133caa11e4e01b1de7e22785d69b80f
Taha Elleuch <tahaeleuch@gmail.com>
2020-02-27 22:09:56 +0000

    final

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -16,7 +16,7 @@ int str_test(char *s1, char *s2, int i, int j)
 	if (s1[i] == '\0' && s2[j] == '*')
 		return (str_test(s1, s2, i, j + 1));
 	if (s2[j] == '*')
-		return (str_test(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+		return (str_test(s1, s2, i + 1, j) || str_test(s1, s2, i, j + 1));
 	return (0);
 }
 /**
