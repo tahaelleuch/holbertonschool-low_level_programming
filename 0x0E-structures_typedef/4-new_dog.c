8591fb511b01b44c4a5d9ff0efda1647a89dd3e0
Taha Elleuch <tahaeleuch@gmail.com>
2020-03-09 22:47:04 +0000

    dogs

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * new_dog - creates a new dog
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Return: struct the new dog
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	return (NULL);
+}
