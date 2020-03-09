f6ff8edf1c37c1ef8433531f60370fac6f6907f9
Taha Elleuch <tahaeleuch@gmail.com>
2020-03-09 21:46:21 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dogs infos
+ * @name: char name of the dog
+ * @age: int his age
+ * @owner: char his owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
