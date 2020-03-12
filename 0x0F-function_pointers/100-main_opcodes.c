4dc5d585839a277eee4c381079ec79e2bca13150
Taha Elleuch <tahaeleuch@gmail.com>
2020-03-12 22:18:57 +0000

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,17 +8,30 @@
 */
 int main(int argc, char **argv)
 {
-	int n;
+	int n, i;
+	char *charg;
 
 	if (argc != 2)
 	{
 		puts("Error\n");
 		exit(1);
 	}
-	else if (argv[1][0] == '-')
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		puts("Error\n");
 		exit(2);
 	}
+	charg = (char *)main;
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%.2hhx", charg[i]);
+			if (i < n - 1)
+				printf(" ");
+		}
+	}
+	printf("\n");
 	return (0);
 }
