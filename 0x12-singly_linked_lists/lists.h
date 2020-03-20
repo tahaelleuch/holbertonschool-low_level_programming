f47b9240a8f278a0719bf1b5ce3f9b74fddcf18b
Taha Elleuch <tahaeleuch@gmail.com>
2020-03-20 16:09:53 +0000

    lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+
+/**
+ * struct list_t - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: adresse of the next node
+*/
+typedef struct list_t
+{
+	char *str;
+	unsigned long len;
+	struct list_t *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
