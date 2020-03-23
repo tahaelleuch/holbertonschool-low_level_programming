28494b6fc97d905ee4414cb07947ed71121fabb5
Taha Elleuch <tahaeleuch@gmail.com>
2020-03-23 23:36:26 +0000

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,12 @@ void free_listint2(listint_t **head)
 	int i;
 	listint_t *mid;
 
+	if (head == NULL)
+		return;
 	for (i = 0; head; i++)
 	{
-		mid = *head;
-		*head = (*head)->next;
-		free(mid);
+		mid = (*head)->next;
+		free(*head);
+		*head = mid;
 	}
 }
