c24cf1e76ede48bb8ddbcf776ad107c9bd81b2c4
Taha Elleuch <tahaeleuch@gmail.com>
2020-03-23 12:41:34 +0000

    README.md

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISH_H
+#define LIST_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * struct listint_t - singly linked list
+ * @n: int
+ * @next: adresse of the next node
+*/
+typedef struct listint_t
+{
+	int n;
+	struct listint_t *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
